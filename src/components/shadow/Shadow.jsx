ed2665b3de4034f3bde19ee2656ebc0c32284378
maria-byh <maria.univ13@gmail.com>
2024-05-06 19:11:56 +0100

    commit

--- src/components/shadow/Shadow.jsx
+++ src/components/shadow/Shadow.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 export default function Shadow() {
   return (
     <div>
+      <div 
+        className='w-52 h-52 bg-slate-500 rounded-2xl'
+        draggable
+      >
+        hello
+      </div>
       
     </div>
   )
